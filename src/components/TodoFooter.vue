aa328a4ca6f0de8c9070102c43cf326dc9c4dca2
jihyun.kwon <jihyun.kwon@fixelsoft.com>
2020-04-07 10:04:52 +0900

    todo 컴포넌트 기본 코드 틀 잡기

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -1,13 +1,11 @@
 <template>
-
+  <div>footer</div>
 </template>
 
 <script>
-    export default {
-        name: "TodoFooter"
-    }
+  export default{
+
+  }
 </script>
-
-<style scoped>
-
+<style>
 </style>
